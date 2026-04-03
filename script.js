fcc015f2c3d5536ffd490dd9cc38b64b4b16b35b
alerossi <alessandrorossi131008@gmail.com>
2026-04-03 11:34:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function controllaQuiz() {
     let punteggio = 0;
-    // Seleziona solo le risposte cliccate
     let risposte = document.querySelectorAll('input[type="radio"]:checked');
 
-    // Controlla se l'utente ha risposto a tutte le domande (sono 3)
     if (risposte.length < 3) {
         alert("Per favore, rispondi a tutte le domande prima di inviare!");
         return;
     }
 
-    // Ciclo per controllare quante sono corrette
     risposte.forEach(risposta => {
         if (risposta.value === "corr") {
             punteggio++;
